97ba8e520d0c7f5ff81098823428d2c3c9edd4eb
Dan Kahle <dan.kahle@gmail.com>
2017-11-27 16:04:50 -0800

    toolbar

--- src/app/shared/material-index/material-index.module.ts
+++ src/app/shared/material-index/material-index.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {
   MatButtonModule, MatDialogModule, MatExpansionModule, MatInputModule,
-  MatProgressBarModule
+  MatProgressBarModule, MatToolbarModule
 } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserAnimationsModule, MatButtonModule, MatProgressBarModule, MatDialogModule, MatInputModule, MatExpansionModule],
-  exports: [BrowserAnimationsModule, MatButtonModule, MatProgressBarModule, MatDialogModule, MatInputModule, MatExpansionModule],
+  imports: [BrowserAnimationsModule, MatButtonModule, MatProgressBarModule, MatDialogModule, MatInputModule, MatExpansionModule,
+    MatToolbarModule],
+  exports: [BrowserAnimationsModule, MatButtonModule, MatProgressBarModule, MatDialogModule, MatInputModule, MatExpansionModule,
+    MatToolbarModule],
 })
 export class MaterialIndexModule {
 }
