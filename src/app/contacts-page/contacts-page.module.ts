02327316fec728a849af7e059e8745496e2ec1ed
Dan Kahle <dan.kahle@gmail.com>
2017-12-01 15:51:57 -0800

    working on search

--- src/app/contacts-page/contacts-page.module.ts
+++ src/app/contacts-page/contacts-page.module.ts
@@ -7,13 +7,13 @@ import {ContactListComponent} from './main/contact-list/contact-list.component';
 import {ContactDetailComponent} from './main/contact-detail/contact-detail.component';
 import {ContactDetailRouteComponent} from './main/contact-detail/contact-detail-route.component';
 import {ContactListItemComponent} from './main/contact-list-item/contact-list-item.component';
-import {HeaderComponent} from './h1/header/header.component';
+import {HeaderComponent} from './header/header/header.component';
 import {LeftnavComponent} from './leftnav/leftnav/leftnav.component';
 import {LeftnavLabelComponent} from './leftnav/leftnav-label/leftnav-label.component';
 import {EditLabelComponent} from './leftnav/edit-label/edit-label.component';
 import {DeleteLabelComponent} from './leftnav/delete-label/delete-label.component';
 import {ContactsPageService} from './contacts-page.service';
-import {ContactSearchComponent} from './h1/contact-search/contact-search.component';
+import {ContactSearchComponent} from './header/contact-search/contact-search.component';
 
 @NgModule({
   imports: [
