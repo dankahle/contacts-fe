b67ed4c0255b7e0ef2041e4d994d57656d42157e
Dan Kahle <dan.kahle@gmail.com>
2017-12-02 12:16:40 -0800

    circular dep problems ongoing

---
+++ src/app/contacts-page/main/contact-edit/contact-edit.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'dk-contact-edit',
+  templateUrl: './contact-edit.component.html',
+  styleUrls: ['./contact-edit.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class ContactEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
